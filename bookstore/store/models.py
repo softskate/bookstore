a58f1380546ba02d584dc1b257da3c33fe98d9bf
softskate <lador.org@gmail.com>
2024-06-03 19:26:38 +0500

    DB modeled

--- bookstore/store/models.py
+++ bookstore/store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='books/')
+    category = models.ForeignKey(Category, related_name='books', on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name='books')
+
+    def __str__(self):
+        return self.title
